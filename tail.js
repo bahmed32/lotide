48e04a5b81564986e8b0ac78b5e10eaeb22b6ad9
Bilqis Ahmed <bilqisahmed@Bilqiss-MBP.vc.shawcable.net>
2023-10-19 15:00:09 -0700

    New Head File

---
+++ tail.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  if( actual === expected) {
+  return (`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+  if(actual !== expected) {
+    return (`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+  
+  };
+  
+  
+ const tail = function(array) {
+  return array.slice()
+  
+  
+ };
+  // Test Case: Check the original array 
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+console.log(assertEqual(tail(words), ["Lighthouse", "Labs"]))
