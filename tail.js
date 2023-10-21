903a7378a9210cfb20cd736a38b57c4ffbd99d97
Bilqis Ahmed <bilqis.ali32@gmail.com>
2023-10-21 21:26:24 +0000

    tail update

--- tail.js
+++ tail.js
@@ -1,4 +1,4 @@
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     return (`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   }
@@ -9,13 +9,16 @@ const assertEqual = function (actual, expected) {
 };
 
 
-const tail = function (array) {
-  return array.slice(1)
-
-
+const tail = function(array) {
+  if (array.length <= 1) {
+    return [];
+  }
+  return array.slice(1);
 };
+
+
 // Test Case: Check the original array 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-console.log(assertEqual(tail(words), ["Lighthouse", "Labs"]))
+console.log(assertEqual(words.length, 3)); // original array should still have 3 elements!
+console.log(assertEqual(tail(words), ["Lighthouse", "Labs"]));
