ac94655f405d4db10ca4e2f1e6a7b2017509357a
Bilqis Ahmed <bilqis.ali32@gmail.com>
2023-10-21 17:38:37 +0000

    countLetters update

---
+++ countLetter.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    return (`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+  return (`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+
+};
+
+const countLetters = function(str) {
+  const result = {};
+
+  for (const letter of str) {
+
+    if (!result[letter]) {
+      result[letter] = 0;
+
+    }
+    result[letter]++;
+  }
+  return result;
+
+};
+
+const result1 = countLetters("hello");
+console.log(assertEqual(result1["h"], 1));  
+console.log(assertEqual(result1["e"], 1));  
+console.log(assertEqual(result1["l"], 2));  
+console.log(assertEqual(result1["o"], 1));
